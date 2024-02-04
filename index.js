41b14ecd6a35042e53e960f115f38ccb5ff52664
Shriyash <mandavekarshriyash@gmail.com>
2024-02-04 19:26:55 +0530

    add the remaining part

--- index.js
+++ index.js
@@ -11,19 +11,28 @@ app.use(bodyParser.json());
 
 const getStartupData = require('./routes/get-routes/getStartupData')
 const getAllStartupsData = require('./routes/get-routes/getAllStartupData')
-const checkInvestorPassword = require('./routes/post-routes/checkInvestorpassword')
-const checkStartUpPassword = require('./routes/post-routes/checkStartupPassword')
-app.use(checkStartUpPassword)
+const incomingRequests = require('./routes/get-routes/incomingRequests')
+const requestsUser = require('./routes/get-routes/requestsUser')
 app.use(getStartupData)
-app.use(checkInvestorPassword)
 app.use(getAllStartupsData)
+app.use(incomingRequests)
+app.use(requestsUser)
 
 const addInvestor = require('./routes/post-routes/addInvestor')
 const addStartup = require('./routes/post-routes/addStartup')
 const updateSales = require('./routes/post-routes/updateSales')
+const checkInvestorPassword = require('./routes/post-routes/checkInvestorpassword')
+const checkStartUpPassword = require('./routes/post-routes/checkStartupPassword')
+const submitInterest = require('./routes/post-routes/submitInterest')
+const changeRequestStatus = require('./routes/post-routes/changeRequestStatus')
+
 app.use(addInvestor)
 app.use(addStartup)
+app.use(checkInvestorPassword)
+app.use(checkStartUpPassword)
 app.use(updateSales)
+app.use(submitInterest)
+app.use(changeRequestStatus)
 
 const dbName = "Fundrev";
 mongoose
